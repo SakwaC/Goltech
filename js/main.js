35c8bd18ad5a872709a6796360e0383f1d9eabaa
SakwaC <sakwacyprian0@gmail.com>
2025-01-24 16:58:02 +0300

    made some changes

--- js/main.js
+++ js/main.js
@@ -1,35 +1,34 @@
 (function ($) {
     "use strict";
 
-
     jQuery(document).ready(function ($) {
-
-
         /*---------------------------------------------*
         * Carousel
         ---------------------------------------------*/
         $('#Carousel').carousel({
-                interval: 5000,
-                item: 2
-            })
-            /*------------------------*/
+            interval: 5000,
+            item: 2
+        });
 
+        /*---------------------------------------------*
+        * Smooth Scrolling
+        ---------------------------------------------*/
+        $('a[href^="#"]').on('click', function (event) {
+            var target = $(this.getAttribute('href'));
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').stop().animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+            }
+        });
     });
 
     /*---------------------------------------------*
         * STICKY scroll
     ---------------------------------------------*/
-
     $.localScroll();
 
-    /**************************/
-
-
-    jQuery(window).load(function () {
-
-
-    });
-
-
+    jQuery(window).load(function () {});
 
 }(jQuery));
